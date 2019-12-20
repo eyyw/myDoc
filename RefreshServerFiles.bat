635f29e92b85ceea00f014e007766a134df2b976
yyw <yang.yawei@aol.com>
2019-12-20 17:50:55 +0900

    Add files via upload

--- RefreshServerFiles.bat
+++ RefreshServerFiles.bat
@@ -1,34 +1,52 @@
 echo off
+setlocal enabledelayedexpansion 
 
-REM FILE_1 = 設計QA
-SET FILE_PATH_1=\\26.131.131.1\pj_share\20_Fact展開（一般メタル）\01.成果物\21.外部設計\QA\
-SET FILE_NAME_1=QA一覧v3.xlsx
+set  wkFile01[0]="\\26.43.131.1\public\2_MISSION開発\6_職責管理の追加開発\仕様検討一覧.xlsx"
+set  wkFile01[1]="\\26.43.131.1\public\2_MISSION開発\0_計画\工数管理.xlsx"
+set  wkFile01[2]="\\26.43.131.1\public\2_MISSION開発\6_職責管理の追加開発\職責管理_レビュー記録表.xlsx"
+set  wkFile01[3]=""
+set  wkFile01[4]=""
+set  wkFile01[5]=""
+set  wkFile01[6]=""
+set  wkFile01[7]=""
+set  wkFile01[8]=""
+set  wkFile01[9]=""
+set wkFile01[10]=""
+set PATH_LOC_WK_01=C:\yyw\500_Person\500_work\20191119_EBS\6_職責管理の追加開発\
 
-REM FILE_2 = 開発QA
-SET FILE_PATH_2=\\26.131.131.1\pj_share\20_Fact展開（一般メタル）\01.成果物\23.開発\QA\
-SET FILE_NAME_2=QA一覧.xlsx
+set  wkFile02[0]=""
+set  wkFile02[1]=""
+set  wkFile02[2]=""
+set  wkFile02[3]=""
+set  wkFile02[4]=""
+set  wkFile02[5]=""
+set  wkFile02[6]=""
+set  wkFile02[7]=""
+set  wkFile02[8]=""
+set  wkFile02[9]=""
+set wkFile02[10]=""
+set PATH_LOC_WK_02=C:\yyw\500_Person\500_work\
 
-REM FILE_3 = 設計レビュー
-SET FILE_PATH_3=\\26.131.131.1\pj_share\20_Fact展開（一般メタル）\01.成果物\21.外部設計\設計レビュー\
-SET FILE_NAME_3=ﾚﾋﾞｭｰ記録_一般メタル_外部設計.xlsx
+REM とりあえず50回ループする
+REM for /l %%f in (0,1,50) do (
+REM    if not "a!wkFile01[%%f]!" == "a" (
+REM        call :startCopy !wkFile01[%%f]! %PATH_LOC_WK_01%
+REM    )
+REM    if not "a!wkFile02[%%f]!" == "a" (
+REM        call :startCopy !wkFile02[%%f]! %PATH_LOC_WK_02%
+REM    )
+REM )
 
-REM FILE_4 = プロジェクト管理
-SET FILE_PATH_4=\\26.131.131.1\pj_share\20_Fact展開（一般メタル）\99.WORK\you\20190704_開発関連資料\
-SET FILE_NAME_4=Git上のプロジェクト管理.xlsx
+REM とりあえず10回ループする
+for /l %%f in (0,1,10) do (
+   if exist !wkFile01[%%f]! (
+       call :startCopy !wkFile01[%%f]! %PATH_LOC_WK_01%
+   )
+   if exist !wkFile02[%%f]! (
+       call :startCopy !wkFile02[%%f]! %PATH_LOC_WK_02%
+   )
+)
 
-REM FILE_5 = 受入不具合
-SET FILE_PATH_5=\\26.131.131.1\pj_share\20_Fact展開（一般メタル）\01.成果物\22.テスト\01.単体テスト\02.単体テスト（オフショア）\
-SET FILE_NAME_5=オフショア_テスト結果(不具合票（一覧）(1.0.6).xls
-
-
-
-SET PATH_LOC_WK=C:\yyw\500_Person\500_work\
-
-call :startCopy %FILE_PATH_1% %PATH_LOC_WK% %FILE_NAME_1%
-call :startCopy %FILE_PATH_2% %PATH_LOC_WK% %FILE_NAME_2%
-call :startCopy %FILE_PATH_3% %PATH_LOC_WK% %FILE_NAME_3%
-call :startCopy %FILE_PATH_4% %PATH_LOC_WK% %FILE_NAME_4%
-call :startCopy %FILE_PATH_5% %PATH_LOC_WK% %FILE_NAME_5%
 
 
 pause
@@ -38,13 +56,12 @@ goto :EOF
 
 rem %1=cp_path_from,%2=cp_path_to,%3=cp_file_nm
 :startCopy
-  echo copy %1%3 %2
-  copy /Y %1%3 %2
-  rem if errorlevel 0 echo [%3]ファイルのコピーが完了しました。
-  rem if errorlevel 1 echo [%3]ファイル見つかりません。
-  rem if errorlevel 2 echo Ctrl-Cで[%3]ファイルのコピーが中止しました。
-  rem if errorlevel 4 echo [%3]予想外エラーでコピーが中止しました。
-  rem if errorlevel 5 echo [%3]hard disk witring error。
+  rem echo copy /Y %1 %2
+  copy /Y %1 %2
+  if errorlevel 0 echo [%1]ファイルのコピーが完了しました。
+  if errorlevel 1 echo [%1]ファイル見つかりません。
+  if errorlevel 2 echo Ctrl-Cで[%1]ファイルのコピーが中止しました。
+  if errorlevel 4 echo [%1]予想外エラーでコピーが中止しました。
+  if errorlevel 5 echo [%1]hard disk witring error。
   goto :EOF
-
 
